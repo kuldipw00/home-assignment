7af39d226dac2ed3029644936080d97880cd2e83
Kuldip Waghmare <kuldipw00@github.com>
2023-01-19 21:23:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,94 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Pagination from 'react-js-pagination'
+import _ from 'lodash'
+import PostDetail from './PostDetail';
+//import {TextField} from "@mui/material"
+//import "bootstrap/less/bootstrap.less";
 
 function App() {
+  const [postData,setPostData] = useState([])
+  const [LimitedPostRecord,setLimitedPostRecord] = useState([])
+  const [postDetails,setPostDetail] = useState({})
+  const [isPostDetail,setIsPostDetail] = useState(false)
+  const [activePage,setActivePage] = useState(1)
+  const [totalCount,setTotalCount] = useState()
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => res.json())
+      .then((json) => {
+        console.log("JSON",json)
+        setPostData(json);
+        setLimitedPostRecord(json.slice(0,10))
+        setTotalCount(json.length)
+      });
+      
+  },[]);
+  const handlePostSearch = (val) =>{
+    const filteredData = postData.filter(item=>{
+      return item.title.includes(val)
+    })
+    setLimitedPostRecord(filteredData.slice(0,10))
+    setTotalCount(filteredData.length)
+  }
+  const handleSearch = (e) =>{
+    _.debounce(handlePostSearch(e.target.value),500)
+  }
+  //handlePostSearch = _.debounce(handlePostSearch,500)
+  const backButtonClick = () => {
+    setIsPostDetail(false)
+  }
+  const handlePageChange = (pageNumber) =>{
+    console.log("pageNumber",pageNumber)
+    let offset = (pageNumber-1) * 10
+    const limit = pageNumber * 10
+    console.log("offset",offset,limit)
+
+    setLimitedPostRecord(postData.slice(offset,limit))
+    console.log("postLimit",postData.slice(offset,limit))
+    setActivePage(pageNumber)
+
+  }
+
+  const handleOnClick = (e,item) =>{
+    
+    setPostDetail(item)
+    setIsPostDetail(true)
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     {!isPostDetail && <div>
+      <div >
+        <input
+        placeholder='Search Post by Title'
+        className='post-div'
+        onChange={handleSearch}
+        />
+      </div>
+      <div className='post-card-container'>
+        {LimitedPostRecord.map(item=>(
+          <div onClick={(e)=>handleOnClick(e,item)} className="post-card">
+          <label style={{fontWeight:'bold'}}>{item.title}</label>
+          <p>{item.body}</p>
+        </div>
+        ))}
+     
+      </div>
+      <div className='pagination-container'>
+       <Pagination
+        activePage={activePage}
+        itemsCountPerPage={10}
+        totalItemsCount={totalCount}
+        pageRangeDisplayed={5}
+        onChange={handlePageChange}
+       />
+       </div>
+      
+     </div>
+      }
+      {isPostDetail && <PostDetail backButtonClick={backButtonClick} PostDetail={postDetails}></PostDetail>}
     </div>
   );
 }
---
+++ src/PostDetail.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './App.css';
+import Logo from './logo.svg'
+function PostDetail({PostDetail,backButtonClick}) {
+  return (
+    <div>
+    <div style={{textAlign:'center',display:'flex',justifyContent:'center'}}>
+    <button onClick={backButtonClick} style={{margin:'1.5rem',height:'30px',}}>{`< Back`}</button>
+
+        <div className='post-detail'>
+            <label style={{fontWeight:'bold'}}>{PostDetail.title}</label>
+            <p>{PostDetail.body}</p>
+            <img style={{width:'300px'}} alt='' src={Logo}/>
+        </div>
+    </div>
+    </div>
+  )
+}
+
+export default PostDetail
